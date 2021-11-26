3412e4209f5a5aa72ff8a5509257966d7555886b
Michael D. Norman <michael.norman@denormans.com>
2021-11-26 11:11:52 -0600

    chore: fixes after upgrading react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Main } from './pages/Main';
 import { BadRoute } from './pages/BadRoute';
 
 export class App extends React.Component {
   render() {
     return (
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route path="*" component={BadRoute} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="*" element={<BadRoute />} />
+      </Routes>
     );
   }
 }
